a643d12dbafa38813f27596184e5dbf14bcd4e47
KurumiRockbell <kurumi.rockbell@proton.me>
2023-05-19 20:09:50 +0200

    adding profilebanner and profile page

--- app/Views/Partials/MvcUI/topmenu.part.php
+++ app/Views/Partials/MvcUI/topmenu.part.php
@@ -112,7 +112,7 @@ use SYRADEV\app\ReplicateController;
                                             </a>
                                         </li>
                                         <li>
-                                            <a href="#" class="flex items-center text-teal-50 hover:text-indigo-800 group">
+                                            <a href="<?= ReplicateController::getRoute('profile'); ?>" class="flex items-center text-teal-50 hover:text-indigo-800 group">
                                                 <span class="sr-only">Profile</span>
                                                 <svg class="w-4 h-4 mr-2 text-teal-50 group-hover:text-indigo-800" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
